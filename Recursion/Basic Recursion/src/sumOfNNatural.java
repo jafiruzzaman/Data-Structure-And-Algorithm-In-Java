333c04a661fdb3c4e7cbec0ece91bff047148612
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2025-12-08 21:01:59 +0600

    understand & Solve the problem of Sum of N Natural Number using recursion

---
+++ Recursion/Basic Recursion/src/sumOfNNatural.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class sumOfNNatural {
+	static int totalSumOfNNatural(int n){
+		// base-case
+		if (n==1) return 1;
+		// recursive-relation
+		return n+totalSumOfNNatural(n-1);
+	}
+	static void main() {
+		System.out.println("calculate Sum of N Natural Number");
+		System.out.print("Enter the number for N natural Number: ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int result  = totalSumOfNNatural(n);
+		System.out.println("The Sum of N Natural Number is : "+result);
+		sc.close();
+	}
+}

16e875251fc8ff8c4a54c2525ea9d92334e6453e
Mohammad-Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-06-27 20:11:20 +0600

    array: solve linear-search array problem

---
+++ src/concept/arrays/medium/LinearSearchArrayProblem.java
@@ -0,0 +1,33 @@
+/**
+ * Question Name: linear search problem
+ * Question link: https://www.geeksforgeeks.org/problems/search-an-element-in-an-array-1587115621/1
+ */
+package concept.arrays.medium;
+
+import java.util.Scanner;
+
+public class LinearSearchArrayProblem {
+    static int linearSearch(int[] arr, int target) {
+        int res = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) return res = i;
+        }
+        return res;
+    }
+
+    static void main(String[] args) {
+        System.out.println("linear Search problem");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the Array Elements");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the target element you want to find");
+        int target = sc.nextInt();
+        System.out.println(target + " element exist at " + linearSearch(arr, target) + "th index");
+        sc.close();
+    }
+}

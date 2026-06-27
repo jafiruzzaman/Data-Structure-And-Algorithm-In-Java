99ae46bb0f219d70ebb1f1d683a5eeb11f23eb55
Mohammad-Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-06-27 17:13:03 +0600

    array: solve right rotate an array by one places array problem

---
+++ src/concept/arrays/medium/rightRotateAnArrayByOnePlacesProblem.java
@@ -0,0 +1,39 @@
+/**
+ * Question Name: right rotate an array by one places problem
+ * Question link:
+ */
+package concept.arrays.medium;
+
+import java.util.Scanner;
+
+public class rightRotateAnArrayByOnePlacesProblem {
+    static void rightRotateByOnePlaces(int[] arr) {
+        int startVal = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            arr[i - 1] = arr[i];
+        }
+        arr[arr.length - 1] = startVal;
+    }
+
+    static void print(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    static void main(String[] args) {
+        System.out.println("right rotate an array by one places problem");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the Array Elements");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        rightRotateByOnePlaces(arr);
+        print(arr);
+        sc.close();
+    }
+}

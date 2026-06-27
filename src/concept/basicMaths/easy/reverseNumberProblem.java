bbf113d213485b3ad23958dae4a1851d0b8ee04f
Mohammad-Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-06-27 13:07:19 +0600

    basic-math: solve reverse number gfg problem

---
+++ src/concept/basicMaths/easy/reverseNumberProblem.java
@@ -0,0 +1,29 @@
+/**
+ * Question Name: Reverse Number
+ * Question link: https://www.geeksforgeeks.org/problems/reverse-digit0316/1
+ */
+package concept.basicMaths.easy;
+
+import java.util.Scanner;
+
+public class reverseNumberProblem {
+    static int reverseNumber(int num) {
+        if (num < 10) return num;
+        int res = 0;
+        while (num > 0) {
+            res = res * 10 + num % 10;
+            num /= 10;
+        }
+        return res;
+    }
+
+    static void main(String[] args) {
+        System.out.println("Reverse Number Problem");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+        int res = reverseNumber(num);
+        System.out.println("After reversing the number: " + res);
+        sc.close();
+    }
+}

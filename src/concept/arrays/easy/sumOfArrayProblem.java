35334c960a7dc2fe50363120193f55ddcf8333cd
Mohammad-Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-06-27 14:21:34 +0600

    array: solve sum of array sum problem

---
+++ src/concept/arrays/easy/sumOfArrayProblem.java
@@ -0,0 +1,27 @@
+package concept.arrays.easy;
+
+import java.util.Scanner;
+
+public class sumOfArrayProblem {
+    static int sumOfArray(int[] arr) {
+        int res = 0;
+        for (int val : arr) {
+            res += val;
+        }
+        return res;
+    }
+
+    static void main(String[] args) {
+        System.out.println("Sum of Array Problem");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size");
+        int size = sc.nextInt();
+        int[] crr = new int[size];
+        System.out.println("Enter the Array Elements");
+        for (int i = 0; i < crr.length; i++) {
+            crr[i] = sc.nextInt();
+        }
+        System.out.println("Sum of Array is " + sumOfArray(crr));
+        sc.close();
+    }
+}

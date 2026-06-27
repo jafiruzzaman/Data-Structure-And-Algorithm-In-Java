c783b6342b01073b98737c78f9b869d84cbfe098
Mohammad-Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-06-27 09:41:12 +0600

    basics: java program structure with explanation

---
+++ src/concept/java/programStructure/ProgramStructure.java
@@ -0,0 +1,18 @@
+package concept.java.programStructure;
+
+public class ProgramStructure {
+    static void main(String[] args) {
+        System.out.println("Java Program Structure");
+    }
+}
+/*
+ * public: public is an access-modifier
+ * class: class is a user defined blue-print with some attributes & behaviors
+ * ProgramStructure: name of the class
+ * static: static allows to call methods without creating object
+ * void: void is the return type
+ * main(String[] args): method name
+ * System: System is class
+ * out: out is method belongs to System class
+ * println: print with new line
+ */

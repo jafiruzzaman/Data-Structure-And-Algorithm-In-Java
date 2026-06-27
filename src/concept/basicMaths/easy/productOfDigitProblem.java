3e9dda98cfda38ab48bfb498f8a40a840dd86100
Mohammad-Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-06-27 13:28:45 +0600

    basic-math: solve product of digit problem

---
+++ src/concept/basicMaths/easy/productOfDigitProblem.java
@@ -0,0 +1,27 @@
+/**
+ * Question Name: Product of Digits
+ * Question link:
+ */
+package concept.basicMaths.easy;
+
+import java.util.Scanner;
+
+public class productOfDigitProblem {
+    static int productOfDigit(int n) {
+        if (n < 10) return n;
+        int res = 1;
+        while (n > 0) {
+            res = res * (n % 10);
+            n /= 10;
+        }
+        return res;
+    }
+
+    static void main(String[] args) {
+        System.out.println("Product of digit problem");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+        System.out.println("Sum of " + num + " digits is: " + productOfDigit(num));
+    }
+}

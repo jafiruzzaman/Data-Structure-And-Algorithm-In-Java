e02b6a2d44129d1673c8efa7fe351c28d78fb6aa
Mohammad-Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-06-27 13:25:17 +0600

    basic-math: solve sum of digit gfg problem

---
+++ src/concept/basicMaths/easy/sumOfDigitProblem.java
@@ -0,0 +1,29 @@
+/**
+ * Question Name: Sum of Digit Problem
+ * Question link: https://www.geeksforgeeks.org/problems/sum-of-digits1742/1
+ */
+
+package concept.basicMaths.easy;
+
+import java.util.Scanner;
+
+public class sumOfDigitProblem {
+    static int sumOfDigit(int n) {
+        if (n < 10) return n;
+        int res = 0;
+        while (n > 0) {
+            res = res + (n % 10);
+            n /= 10;
+        }
+        return res;
+    }
+
+    static void main(String[] args) {
+        System.out.println("sum of digits problem");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+        System.out.println("Sum of " + num + " digits is: " + sumOfDigit(num));
+        sc.close();
+    }
+}

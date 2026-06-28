47961c9204ff622a03456ed357e6ea7c8b7feda4
Mohammad-Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-06-28 11:52:55 +0600

    matrix: solve largest row-sum matrix problem

---
+++ src/concept/matrix/easy/largestRowSumMatrixProblem.java
@@ -0,0 +1,36 @@
+package concept.matrix.easy;
+
+import java.util.Scanner;
+
+public class largestRowSumMatrixProblem {
+    static int largestRowSum(int[][] matrix) {
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < matrix.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < matrix[0].length; j++) {
+                sum += matrix[i][j];
+            }
+            if (sum > res) res = sum;
+        }
+        return res;
+    }
+
+    static void main(String[] args) {
+        System.out.println("largest row sum in a matrix");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the row");
+        int row = sc.nextInt();
+        System.out.println("Enter the size of the col");
+        int col = sc.nextInt();
+        int[][] arr = new int[row][col];
+        System.out.println("Enter the Array Elements");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter elements for " + i + "th row ");
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = sc.nextInt();
+                ;
+            }
+        }
+        System.out.println("largest row sum is " + largestRowSum(arr));
+    }
+}

b8fc5e77e4e7299270807756b7cbc9b9a1d793b8
Mohammad-Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-06-30 10:30:13 +0600

    binary-search: classic binary-search problem

---
+++ src/concept/binarySearch/easy/ClassicBinarySearch.java
@@ -0,0 +1,47 @@
+/**
+ * Question Name: classic binary search
+ * Question link: https://leetcode.com/problems/binary-search/description/
+ */
+
+package concept.binarySearch.easy;
+
+import java.util.Scanner;
+
+public class ClassicBinarySearch {
+    static int binarySearch(int[] arr, int target) {
+        if (arr.length < 2 && arr[0] == target) return 0;
+        int start = 0, end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) return mid;
+            else if (arr[mid] < target) start = mid + 1; // move pointer
+            else end = mid - 1; // move pointer
+        }
+        return -1;
+    }
+
+    static void print(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    static void main(String[] args) {
+        System.out.println("classic binary search");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size");
+        int size = sc.nextInt();
+        int[] crr = new int[size];
+        System.out.println("Enter array elements");
+        for (int i = 0; i < crr.length; i++) {
+            crr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the target element");
+        int target = sc.nextInt();
+        int res = binarySearch(crr, target);
+        System.out.println(target + " element exist in " + res + "th index");
+        print(crr);
+        sc.close();
+    }
+}

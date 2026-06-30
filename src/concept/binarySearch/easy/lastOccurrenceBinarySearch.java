f139e9d3a7477ca03fbd7af7a7dc17da665c1ab5
Mohammad-Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-06-30 11:31:07 +0600

    binary-search: solve find last occurrence on sorted array problem

---
+++ src/concept/binarySearch/easy/lastOccurrenceBinarySearch.java
@@ -0,0 +1,52 @@
+/**
+ * Question Name:
+ * Question link:
+ */
+package concept.binarySearch.easy;
+
+import java.util.Scanner;
+
+public class lastOccurrenceBinarySearch {
+
+    static void print(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    static int findLastOccurrence(int[] arr, int target) {
+        int start = 0, end = arr.length - 1;
+        int res = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                res = mid;
+                start = mid + 1;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("find the last occurrence of sorted array");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the size");
+            int size = sc.nextInt();
+            int[] crr = new int[size];
+            System.out.println("Enter array elements");
+            for (int i = 0; i < crr.length; i++) {
+                crr[i] = sc.nextInt();
+            }
+            System.out.println("Enter the target element");
+            int target = sc.nextInt();
+            int res = findLastOccurrence(crr, target);
+            System.out.println(target + " element exist in " + res + "th index");
+            print(crr);
+        }
+    }
+}
